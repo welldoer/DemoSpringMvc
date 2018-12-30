f1cee29db52b65baaad36c24fb2472f5a2651dc9
welldoer <welldoer@263.net>
2018-12-31 01:00:32 +0800

    支持 URL 参数为非必输项。

--- src/main/java/net/blogjava/demospringmvc/controller/HelloController.java
+++ src/main/java/net/blogjava/demospringmvc/controller/HelloController.java
@@ -1,14 +1,17 @@
 package net.blogjava.demospringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	
 	@RequestMapping( "/" )
-	public String hello() {
+	public String hello( @RequestParam( defaultValue = "world" ) String userName, Model model ) {
+		model.addAttribute( "message", "Hello, " + userName );
 		return "resultPage";
 	}
 
